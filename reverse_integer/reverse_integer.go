2190fdf324099cf7d3bf6c4879e55baf2db993a6
zjbztianya <spcjv5@gmail.com>
2017-12-05 20:58:52 +0800

    add Leetcode 6. ZigZag Conversion and Leetcode 7. Reverse Integer

---
+++ reverse_integer/reverse_integer.go
@@ -0,0 +1,26 @@
+package reverse_integer
+
+import "math"
+
+func reverse(x int) int {
+	sgn := 1
+	y := int64(x)
+	if x < 0 {
+		sgn = -1
+		y = -y
+		if y > math.MaxInt32 {
+			return 0
+		}
+	}
+
+	var ans int64
+	for y > 0 {
+		ans = ans*10 + y%10
+		y /= 10
+	}
+	ans *= int64(sgn)
+	if ans > math.MaxInt32 || ans < math.MinInt32 {
+		return 0
+	}
+	return int(ans)
+}
---
+++ zigzag_conversion/zigzag_conversion_test.go
@@ -0,0 +1,18 @@
+package zigzag_conversion
+
+import "testing"
+
+func TestConvert(t *testing.T)  {
+	var tests = []struct {
+		s string
+		numRows int
+		output string
+	}{
+		{"PAYPALISHIRING",3,"PAHNAPLSIIGYIR"},
+	}
+	for i,test:=range tests{
+		if got:=convert(test.s,test.numRows); got != test.output{
+			t.Errorf("test case %d: output: %s ans: %s",i,test.output,got)
+		}
+	}
+}
