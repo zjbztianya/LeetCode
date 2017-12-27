8313f118ee1dd7fab7a8e76c733d93f07045606e
zjbztianya <spcjv5@gmail.com>
2017-12-27 20:21:17 +0800

    fix test case

--- search_in_rotated_sorted_array/search_in_rotated_sorted_array_test.go
+++ search_in_rotated_sorted_array/search_in_rotated_sorted_array_test.go
@@ -9,14 +9,14 @@ func TestSearch(t *testing.T) {
 		want   int
 	}{
 		{[]int{}, 5, -1},
-		{[]int{4, 5, 6, 7, 0, 1, 2}, 0, 5},
-		{[]int{4, 5, 6, 1, 2, 3}, 3, 6},
-		{[]int{4, 5, 6, 1, 2, 3}, 4, 1},
+		{[]int{4, 5, 6, 7, 0, 1, 2}, 0, 4},
+		{[]int{4, 5, 6, 1, 2, 3}, 3, 5},
+		{[]int{4, 5, 6, 1, 2, 3}, 4, 0},
 	}
 
 	for i, test := range tests {
 		if got := search(test.input, test.target); got != test.want {
-			t.Errorf("test case %d: output: %d want: %d", i, got, test.input)
+			t.Errorf("test case %d: output: %d want: %d", i, got, test.want)
 		}
 	}
 }
